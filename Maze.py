05f308f38f55b3f5420af9180032f03fbc9e4950
Luke Wiley <Luke Wiley>
2016-09-19 20:07:33 -0400

    passes 9 tests

--- Maze.py
+++ Maze.py
@@ -4,3 +4,16 @@ import turtle
 class Maze():
     def __init__(self):
         self.screen = turtle.Screen()
+        self.turtle = turtle.Turtle()
+        #screen
+        self.screen.screensize(400,400)
+        self.screen.bgcolor("blue")
+        self.screen.register_shape("custom",((10,10),(-10,10),(-10,-10),(10,-10))) #20x20 turtle
+        #turtle
+        self.turtle.shape("custom")
+        self.turtle.color('white')
+        self.turtle.penup()
+        self.turtle.goto(-190,190)
+        #matrix
+        self.matrix = [[1 for i in range(20)] for i in range(20)]
+        self.matrix[0][0] = 0
--- MazeTests.py
+++ MazeTests.py
@@ -33,6 +33,26 @@ class testMaze(unittest.TestCase):
         assert type(self.m.screen) == turtle._Screen
 
     def testTurtleExists(self):
-        assert type(self.m.turtle) == turtle._Turtle
+        assert type(self.m.turtle) == turtle.Turtle
+
+    def testScreenBackgroundIsBlue(self):
+        assert self.m.screen.bgcolor() == "blue"
+
+    def testForMatrix(self):
+        assert len(self.m.matrix) == 20
+
+    def testTurtleIsWhite(self):
+        assert self.m.turtle.color()[0] == 'white' and self.m.turtle.color()[1] == "white"
+
+    def testTurtleIsInUpperLeftHandCorner(self):
+        assert self.m.turtle.position() == (-190,190), "Turtle position is %d, %d" % \
+               (self.m.turtle.position()[0],self.m.turtle.position()[1])
+        
+    def testTurtleMatrixIsZeroInUpperLeftHandCorner(self):
+        assert self.m.matrix[0][0] == 0
+
+    def testScreenSize(self):
+        assert self.m.screen.screensize() == (400,400)
+        
 
 unittest.main()
